59a9dc291613f676d351bb017b9256b646c23e12
happynovice <36851051+happynovice@users.noreply.github.com>
2018-08-20 08:28:38 +0800

    Update cpp_primer_7_4.cpp

--- cpp_primer_7_4.cpp
+++ cpp_primer_7_4.cpp
@@ -3,6 +3,8 @@
 #include<string>
 #include<vector>
 #include<iostream>
+using std::string;
+using std::cin; using std::cout; using std::endl;
 struct sales_data {
 	std::string isbin() const { return bookNo; }
 	sales_data & combine(const sales_data &);
@@ -26,19 +28,30 @@ double sales_data::avg_price() const{
 	}
 
 }
-class person {
-
+struct person {
+	std::string names;
+	std::string addresses;
+	string name() {
+		return this->names;
+	};
+	string address() {
+		return this->addresses;
+	};
 
 };
 int main()
 {
-	sales_data data1, data2;
-	std::cin >> data1.bookNo>>data1.units_sold>>data1.revenue;
-	std::cin >> data2.bookNo>> data2.units_sold >> data2.revenue;
-	if (data1.bookNo == data2.bookNo)
-		return 0;
-	else
-		std::cout << data2.revenue + data1.revenue << " " << data1.units_sold + data2.units_sold << " " << std::endl;
-    return 0;
+	//sales_data data1, data2;
+	//std::cin >> data1.bookNo>>data1.units_sold>>data1.revenue;
+	//std::cin >> data2.bookNo>> data2.units_sold >> data2.revenue;
+	//if (data1.bookNo == data2.bookNo)
+	//	return 0;
+	//else
+	//	std::cout << data2.revenue + data1.revenue << " " << data1.units_sold + data2.units_sold << " " << std::endl;
+	//   return 0;
+	person person1, person2;
+	cin >> person1.names >> person2.names;
+	cout << person2.name() << " " << person1.name()<<endl;
+
 }
 
